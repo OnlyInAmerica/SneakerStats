c5d23c93c76e53352ea432d5cc54f25207dbbf0d
David Brodsky <davidpbrodsky@gmail.com>
2014-12-11 13:26:34 -0800

    override SessionSerializer create

--- api/serializers.py
+++ api/serializers.py
@@ -27,8 +27,23 @@ class IdentityTransmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = IdentityTransmission
 
+
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
         depth = 2
         #fields = ('url', 'username', 'email', 'groups')
+
+    def create(self, validated_data):
+        local_peer_data = validated_data.pop('local_peer')
+        local_peer = Peer.objects.create(**local_peer_data)
+
+        remote_peer_data = validated_data.pop('remote_peer')
+        remote_peer = Peer.objects.create(**remote_peer_data)
+
+        session = Session.objects.create(**validated_data)
+
+        session.local_peer = local_peer
+        session.remote_peer = remote_peer
+
+        return session
