173c7bc7fb1388f1fd8e138a1d52056d56b7a309
David Brodsky <davidpbrodsky@gmail.com>
2014-12-11 12:52:19 -0800

    attempt to make Session serlializer act on nested objects

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,34 @@
 #from django.contrib.auth.models import User, Group
-from models import Session
+from models import Session, Identity, Message, Peer, MessageTransmission, IdentityTransmission
 from rest_framework import serializers
 
 
-class SessionSerializer(serializers.HyperlinkedModelSerializer):
+class IdentitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Identity
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+
+
+class PeerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Peer
+
+
+class MessageTransmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageTransmission
+
+
+class IdentityTransmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IdentityTransmission
+
+class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
+        depth = 2
         #fields = ('url', 'username', 'email', 'groups')
