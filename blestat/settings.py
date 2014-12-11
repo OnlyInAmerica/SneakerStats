9c9107ec0d6aaa126a3b0e0cf1482ea70ea32473
David Brodsky <davidpbrodsky@gmail.com>
2014-12-11 13:10:29 -0800

    Session and Token authentication

--- blestat/settings.py
+++ blestat/settings.py
@@ -90,6 +90,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'PAGINATE_BY': 10
 }
