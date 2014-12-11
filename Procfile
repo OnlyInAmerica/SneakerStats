c34e85cd84db810919cee7d89fdba102b4b5f18e
David Brodsky <davidpbrodsky@gmail.com>
2014-12-10 18:20:06 -0800

    add heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blestat.wsgi --log-file -
---
+++ blestat/local_dev_settings.py
@@ -0,0 +1,11 @@
+__author__ = 'davidbrodsky'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blestat',
+        'USER': 'davidbrodsky',
+        'PASSWORD': 'pigs',
+        'HOST': ''
+    }
+}
